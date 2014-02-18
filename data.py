6434f7e75281e6d31050e3d98a690b1bcfecb961
Sergey Glazyrin <sergey.glazyrin.dev@gmail.com>
2014-02-18 11:49:07 +0200

    moved config to separated file

---
+++ data.py
@@ -0,0 +1,44 @@
+import datetime
+import time
+
+
+def get_timestamp_from_date_string(date_string):
+
+    return time.mktime(datetime.datetime.strptime(date_string, "%d/%m/%Y").timetuple())
+
+percent_to_sell_immediately = 50
+
+currency_exchanges = {
+    get_timestamp_from_date_string('07/10/2013'): 8.17,
+    get_timestamp_from_date_string('07/11/2013'): 8.19,
+    get_timestamp_from_date_string('06/12/2013'): 8.24,
+    get_timestamp_from_date_string('19/12/2013'): 8.26,
+    get_timestamp_from_date_string('31/12/2013'): 8.205,
+    get_timestamp_from_date_string('01/11/2013'): 8.17,
+    get_timestamp_from_date_string('03/12/2013'): 8.223,
+    get_timestamp_from_date_string('17/12/2013'): 8.27,
+    'remainder': 8.205
+}
+
+incoming = {
+    get_timestamp_from_date_string('07/10/2013'): 1000,
+    get_timestamp_from_date_string('07/11/2013'): 0,
+    get_timestamp_from_date_string('06/12/2013'): 0,
+    get_timestamp_from_date_string('19/12/2013'): 0
+}
+
+sold = {
+    get_timestamp_from_date_string('01/11/2013'): 0,
+    get_timestamp_from_date_string('03/12/2013'): 0,
+    get_timestamp_from_date_string('17/12/2013'): 0
+}
+
+
+def get_config_for_calculations():
+    return {
+        'incoming': incoming,
+        'sold': sold,
+        'sell_immediately': percent_to_sell_immediately,
+        'currency_exchanges': currency_exchanges,
+        'custom_percent': 5
+    }
