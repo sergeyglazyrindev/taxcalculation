fe910537c4dc959455f59821cae3f45574f77b3e
Sergey Glazyrin <sergey.glazyrin.dev@gmail.com>
2014-02-18 12:07:35 +0200

    fixes

--- calculate.py
+++ calculate.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python
 
-import datetime
-import time
-
 from tax.tasks import calculate
 import config
 
 
 def run():
-    calculate_customs(config.get_config_for_calculations())
+    calculate(config.get_config_for_calculations())
 
 
 if __name__ == '__main__':
